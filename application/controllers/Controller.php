d557aa7484f6cbb62f5b7615cf1d2423ec39b536
mendrikafitahiana <ramilisonfita24@gmail.com>
2021-09-16 11:16:23 +0300

    commit

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -120,28 +120,41 @@ class Controller extends CI_Controller {
 
 	public function upload()
 	{
-		if ($_FILES['nomfichier']['error']) {
-		  switch ($_FILES['nomfichier']['error']){
-		    case 1: // UPLOAD_ERR_INI_SIZE
-		      echo "Le fichier dépasse la limite autorisée par le serveur (fichier php.ini) !";
-		      break;
-		    case 2: // UPLOAD_ERR_FORM_SIZE
-		      echo "Le fichier dépasse la limite autorisée dans le formulaire HTML !";
-		      break;
-		    case 3: // UPLOAD_ERR_PARTIAL
-		      echo "L'envoi du fichier a été interrompu pendant le transfert !";
-		      break;
-		    case 4: // UPLOAD_ERR_NO_FILE
-		      echo "Le fichier que vous avez envoyé a une taille nulle !";
-		      break;
-		  	}
-		}else{
-	   		$nom = $_FILES['nomfichier']['name'];
-	   		$nomUpload = $_FILES['nomfichier']['tmp_name'];
-	   		// var_dump($nom);
-		  $nomdestination = "F:/Info Mendrika/ITU LECONS/Rojo/PHP/05-php-S1/UwAmp/www/hack/Hackathon/application/upload/".$nom."";
-		  move_uploaded_file($nomUpload, $nomdestination);
-		  echo "upload vita";
+		if ($$_FILES["nomfichier"]["size"] < 20000) 
+		{
+			if ($_FILES["nomfichier"]["error"] > 0)
+			{
+				switch ($_FILES['nomfichier']['error'])
+				{
+					case 1: // UPLOAD_ERR_PARTIAL
+					  echo "Tsy tontonsa hatramin'ny farany ny fangatahanao !";
+					  break;
+					case 2: // UPLOAD_ERR_NO_FILE
+					  echo "Tsy misy lanjany ny  fampitanao !";
+					  break;
+				}
+			}
+		  else
+			{
+				$nom = $_FILES['nomfichier']['name'];
+				$nomUpload = $_FILES['nomfichier']['tmp_name'];
+				// var_dump($nom);
+				$nomdestination = "F:/Info Mendrika/ITU LECONS/Rojo/PHP/05-php-S1/UwAmp/www/hack/Hackathon/application/upload/".$nom."";
+				move_uploaded_file($nomUpload, $nomdestination);
+				echo "tontonsa ny fampitanao";
+			
+				if (file_exists("upload/" . $_FILES["file"]["name"]))
+				{
+					echo "efa misy anarana mitovy amin'ny ".$_FILES["file"]["name"]." ao";
+				}
+			}
+			
 		}
+		else
+		{
+			echo "tsy mety ny lahatsoratra ampitanao";
+		}
+		
 	}
+	
 }
--- application/models/Picture.php
+++ application/models/Picture.php
@@ -25,16 +25,6 @@
         return $retour;
     }
 
-    public function file_get_contents_curl($url) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
-        curl_setopt($ch, CURLOPT_URL, $url);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
-
     public function getPrincipalPics($photo)
     {
         $allpath = "";
